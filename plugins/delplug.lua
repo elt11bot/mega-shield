3771f57ef49f0c87707aad475b24badd3da1522d
elt11bot <aryaneskandari1381@gmail.com>
2016-07-21 22:49:32 +0100

    Create delplug.lua

---
+++ plugins/delplug.lua
@@ -0,0 +1,1 @@
+local function run(msg, matches) if matches[1] == "delplugin" and is_sudo(msg) then text = io.popen("cd plugins && rm "..matches[2]..".lua") return "پلاگین ["..matches[2].."] با موفقیت حذف شد" end end return { patterns = { '^[!/](delplugin) (.*)$' }, run = run, } 
